3d5f391afdcea215377e7b467ac13a88110b3791
mxcom <56640416+mxcom@users.noreply.github.com>
2022-05-09 19:08:34 +0200

    Update 2_ApproachLights.ino

--- 3_Arduino/2_ApproachLights/2_ApproachLights.ino
+++ 3_Arduino/2_ApproachLights/2_ApproachLights.ino
@@ -17,35 +17,35 @@ void loop() {
     digitalWrite(5, HIGH);
     //hier braucht es long ... 60.5 wird absgeschnitten -> evtl delayMicrosec
     //https://www.arduino.cc/reference/en/language/functions/time/delaymicroseconds/
-    delay(60.5);   
+    delay(100);   
     digitalWrite(6, LOW);
     delay(2);
     digitalWrite(6, HIGH);
-    delay(60.5);
+    delay(100);
     digitalWrite(7, LOW);
     delay(2);
     digitalWrite(7, HIGH);
-    delay(60.5);
+    delay(100);
     digitalWrite(8, LOW);
     delay(2);
     digitalWrite(8, HIGH);
-    delay(60.5);
+    delay(100);
     digitalWrite(9, LOW);
     delay(2);
     digitalWrite(9, HIGH);
-    delay(60.5);
+    delay(100);
     digitalWrite(10, LOW);
     delay(2);
     digitalWrite(10, HIGH);
-    delay(60.5); 
+    delay(100); 
     digitalWrite(11, LOW);
     delay(2);
     digitalWrite(11, HIGH);
-    delay(60.5);
+    delay(100);
     digitalWrite(12, LOW);
     delay(2);
     digitalWrite(12, HIGH);
-    delay(60.5);
+    delay(100);
   } else {
     digitalWrite(5, HIGH);
     digitalWrite(6, HIGH);
