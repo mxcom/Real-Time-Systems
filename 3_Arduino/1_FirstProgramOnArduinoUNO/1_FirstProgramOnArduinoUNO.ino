b81d2d20ff55714280a6600af0266b10852e9ec9
mxcom <mxprogramming@protonmail.com>
2022-05-09 08:25:44 +0200

    Update 1_FirstProgramOnArduinoUNO.ino

--- 3_Arduino/1_FirstProgramOnArduinoUNO/1_FirstProgramOnArduinoUNO.ino
+++ 3_Arduino/1_FirstProgramOnArduinoUNO/1_FirstProgramOnArduinoUNO.ino
@@ -1,11 +1,26 @@
 void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(LED, OUTPUT);
+  pinMode(1, OUTPUT);
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
 }
 
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);
+  digitalWrite(1, LOW);
+  delay(2000);
+  digitalWrite(2, LOW);
+  delay(2000);
+  digitalWrite(3, LOW);
+  delay(2000);
+  digitalWrite(4, LOW);
   delay(1000);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(1000);
+  delay(2000);
+  digitalWrite(2, HIGH);
+  delay(2000);
+  digitalWrite(4, HIGH);
+  delay(2000);
+  digitalWrite(3, HIGH);
+  delay(2000);
+  digitalWrite(1, HIGH);
+  delay(2000);
 }
