9403641961bed5ad3580fbc32da0c08258e65560
mxcom <mxprogramming@protonmail.com>
2022-05-25 15:48:41 +0200

    Update 3_ReactionTest.ino

--- 3_Arduino/3_ReactionTest/3_ReactionTest.ino
+++ 3_Arduino/3_ReactionTest/3_ReactionTest.ino
@@ -1,16 +1,31 @@
+// LOW = Not Pressed
+// HIGH = Pressed
+const int button1 = 1;
+const int button2 = 2;
+
+int buttonState = 0;
+int lastButtonState = 0;
+
 void setup() {
-  // put your setup code here, to run once:
+  pinMode(button1, INPUT);
+  // pinMode(2, INPUT_PULLUP);
   Serial.begin(9600);
   Serial.println("Hello this is a reaction Test of your name");
-  pinMode(1, INPUT_PULLUP);
-  pinMode(2, INPUT_PULLUP);
-
 }
 
 void loop() {
-  if(digitalRead(1) == LOW) {
-    Serial.println("1: Pressed");
-  } else {
-    Serial.println("2: Pressed");
+  buttonState = digitalRead(button1);
+
+  // compare the buttonState to its previous state
+  if (buttonState != lastButtonState) {
+    // if the state has changed, increment the counter
+    if (buttonState == HIGH) {
+      Serial.println("pressed");
+    } else {
+      // if the current state is LOW then the button went from on to off:
+      Serial.println("not pressed");
+    }
   }
+  // save the current state as the last state, for next time through the loop
+  lastButtonState = buttonState;
 }
