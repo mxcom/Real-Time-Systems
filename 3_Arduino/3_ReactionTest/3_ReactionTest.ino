21df6c9b01cdfc051bacb46f4ac125c47169f659
mxcom <56640416+mxcom@users.noreply.github.com>
2022-05-25 17:19:20 +0200

    Update 3_ReactionTest.ino

--- 3_Arduino/3_ReactionTest/3_ReactionTest.ino
+++ 3_Arduino/3_ReactionTest/3_ReactionTest.ino
@@ -1,31 +1,50 @@
-// LOW = Not Pressed
-// HIGH = Pressed
-const int button1 = 1;
-const int button2 = 2;
+// LOW  = button not pressed
+// HIGH = button pressed
+
+const int button1 = 2;
+const int button2 = 4;
+const int led = 13;
+unsigned long time1;
+unsigned long time2;
+unsigned long diff;
+float diffSec;
 
 int buttonState = 0;
 int lastButtonState = 0;
 
 void setup() {
   pinMode(button1, INPUT);
-  // pinMode(2, INPUT_PULLUP);
+  pinMode(button2, INPUT);
+  pinMode(led, OUTPUT);
   Serial.begin(9600);
   Serial.println("Hello this is a reaction Test of your name");
 }
 
 void loop() {
-  buttonState = digitalRead(button1);
+  while(digitalRead(button1) == LOW) { /* wait */ }
 
-  // compare the buttonState to its previous state
-  if (buttonState != lastButtonState) {
-    // if the state has changed, increment the counter
-    if (buttonState == HIGH) {
-      Serial.println("pressed");
-    } else {
-      // if the current state is LOW then the button went from on to off:
-      Serial.println("not pressed");
-    }
-  }
-  // save the current state as the last state, for next time through the loop
-  lastButtonState = buttonState;
+  time1 = micros();
+  Serial.print("time 1: ");
+  Serial.print(time1);
+  Serial.println(" ms");
+
+  while(digitalRead(button2) == LOW) { /* wait */ }
+
+  time2 = micros();
+  Serial.print("time 2: ");
+  Serial.print(time2);
+  Serial.println(" ms");
+  
+  diff = (time2 - time1);
+  
+  Serial.print("time difference: ");
+  Serial.print(diff);
+  Serial.println(" ms");
+  
+  diffSec = (float)(diff / 1e6);
+  
+  Serial.print("time difference: ");
+  Serial.print(diffSec, 6);
+  Serial.println(" s");
+  
 }
